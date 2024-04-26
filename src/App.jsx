39fdba6d9ed4c911f8d0548b6eb4f0fc59babb07
Mehemmed <LENOVA-NB@LENOVA>
2024-04-26 11:42:07 +0400

    UserLogin added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
 import { Profile } from './companents/Profile/Profile'
+import { Form } from './companents/UserLogin/Form/Form'
 
 function App() {
 
   return (
-    <>
-      <Profile/>
-    </>
+    <body>
+      {/* <Profile/> */}
+      <Form/>
+    </body>
   )
 }
 
