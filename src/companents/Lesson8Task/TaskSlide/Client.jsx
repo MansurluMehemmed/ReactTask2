70b2004c03ddd06d4af521eb13a3c54ffe99ccbc
Mehemmed <LENOVA-NB@LENOVA>
2024-05-02 10:38:39 +0400

    Added Lesson8Task

--- src/companents/Lesson8Task/TaskSlide/Client.jsx
+++ src/companents/Lesson8Task/TaskSlide/Client.jsx
@@ -4,7 +4,7 @@ const ClientCard = (props) => {
   const { name, phone, card } = props;
 
   return (
-    <div  className={(card&&name&&phone)&&'background-gray' || card?'':'background-yellow'}  style={{ width: "18rem",color:'black' }}>
+    <div  className={(card&&name&&phone)?'background-gray':'' || card?'':'background-yellow'}  style={{ width: "18rem",color:'black' }}>
       <div className="card-body">
         <h5 className="card-title">
           Имя клиента: { name!==''?name : 'Anonimus'}
